b6b582506c0e6e79d09c37142965b6cac4c3e4b5
Kevin Gardner <emailklg2013@gmail.com>
2021-08-29 04:52:27 -0700

    my first program

---
+++ MyOwnJavaProgram.java
@@ -0,0 +1,8 @@
+public class MyOwnJavaProgram {
+
+    public static void main(String[] args) {
+        System.out.println("My name is Kevin Gardner");
+        System.out.println("I am 61 years old");
+        System.out.println("My hometown is Los Angeles");
+    }
+}
